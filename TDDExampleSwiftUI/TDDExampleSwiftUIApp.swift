3dd633557003de21f72907561432fac7b814185f
Eunice <jes.e.0927@gmail.com>
2024-03-12 17:49:19 +0900

    Initial Commit

---
+++ TDDExampleSwiftUI/TDDExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDExampleSwiftUIApp.swift
+//  TDDExampleSwiftUI
+//
+//  Created by Eunsu JEONG on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TDDExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
